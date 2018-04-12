b6a82229ebfa17520cd47dfbccb159162b9eecfe
萊恩(Ryan) <ryanlin777@gmail.com>
2018-04-12 18:57:02 +0800

    Update centos7_install_nginx_yum.sh

--- centos7_install_nginx_yum.sh
+++ centos7_install_nginx_yum.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-#取消selinux功能關閉firewalld
+#取消selinux功能
 setenforce 0
 sed -i "s/SELINUX=enforcing/SELINUX=disabled/g" /etc/selinux/config
 
-# 讓 yum 能找到 nginx
-echo [nginx] >> /etc/yum.repos.d/nginx.repo
-echo name=nginx repo >> /etc/yum.repos.d/nginx.repo
-echo baseurl=http://nginx.org/packages/centos/6/$basearch/ >> /etc/yum.repos.d/nginx.repo
-echo gpgcheck=0 >> /etc/yum.repos.d/nginx.repo
-echo enabled=1 >> /etc/yum.repos.d/nginx.repo
-
+# 讓 yum  添加CentOS 7 Nginx yum资源库
+sudo rpm -Uvh http://nginx.org/packages/centos/7/noarch/RPMS/nginx-release-centos-7-0.el7.ngx.noarch.rpm
 #安裝
 yum install nginx -y
 
