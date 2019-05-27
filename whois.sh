7b8acd26345714b456ab181c6d178d73e4a3b6a2
j777 <ryanlin777@gmail.com>
2019-05-27 09:34:58 +0800

    Update whois.sh

--- whois.sh
+++ whois.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+#刪除舊report
 rm -rf /home/whois_report.txt
 
+#讀取list內url
 filename='/home/whois_list.txt'
 
+#顯示查詢時間
+date  +"%Y-%m-%d %H:%M.%S"
 exec < $filename
 while read line
 
@@ -11,13 +15,16 @@ do
 
 #whois查詢check_name.txt
 echo $line  >> /home/whois_report.txt
- whois $line 2>&1 | grep 'Expiration Date:'  >> /home/whois_report.txt
+ whois $line 2>&1 | grep ' Expiration Date:'  >> /home/whois_report.txt
 
 #cat /home/whois_report.txt
-
 #whois $line 2>&1 | grep "Domain Name:\|Creation Date: \|Registry Expiry Date:\|Updated Date:\|Name Server:"  >> /home/whois_report.txt
 #echo $line # 一行一行印出內容
 
     #echo 'a' $line # 印出 "a $line" 此行的內容, 可另外作特別處理.
+
 done
+
+cat /home/whois_report.txt
+
 
